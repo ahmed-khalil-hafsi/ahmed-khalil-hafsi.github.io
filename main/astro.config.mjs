fdadf863564fb7a1539bc237da0078d42aa654b7
Ahmed K. Hafsi <25119333+ahmed-hafsi@users.noreply.github.com>
2025-03-29 12:42:36 +0800

    small changes

--- main/astro.config.mjs
+++ main/astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahmed-khalil-hafsi.github.io',
+  site: 'https://ahmedhafsi.com',
   integrations: [
     tailwind({
       config: {
